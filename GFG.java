fb125e1ca197d475e4948eb9f3b21a1f4097ac81
Hemanth kumar D <hemanthkumard121@gmail.com>
2025-08-08 20:11:33 +0530

    Add files via upload

--- GFG.java
+++ GFG.java
@@ -1,12 +1,21 @@
-package Program4;
-//Program to determine the IP address and host name of the local computer.
+package Program3;
+//Program to find the IP address of the given hostname:
 
 import java.io.*;
-import java.net.InetAddress;
+import java.util.*;
+import java.net.*;
 public class GFG {
-public static void main(String[] args) throws Exception {
-InetAddress addr = InetAddress.getLocalHost();
-System.out.println("Local HostAddress: "+ addr.getHostAddress());
-System.out.println("Local host name: "+ addr.getHostName());
+public static void main(String[] args) {
+String host = "www.google.com"; 
+try {
+InetAddress[] iaddress = InetAddress.getAllByName(host);
+for (InetAddress ipaddresses : iaddress)
+{
+System.out.println(ipaddresses.toString());
+}
+}
+catch (UnknownHostException e) {
+System.out.println(e);
+}
 }
 }
